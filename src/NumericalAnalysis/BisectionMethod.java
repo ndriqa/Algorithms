9fddaf9bb0f6d33c9f3eed102a34201d4202720e
ndriqa <ndriqimrrahmani@hotmail.com>
2020-01-26 19:55:03 +0100

    Added Bisection Mode algorithm for finding root of a function

---
+++ src/NumericalAnalysis/BisectionMethod.java
@@ -0,0 +1,66 @@
+package NumericalAnalysis;
+
+import java.util.function.Function;
+
+public class BisectionMethod {
+    private double endpointA, endpointB;
+    private double tolerance;
+    private int N;
+
+    private Double root;
+    /**
+     * @param a start of interval.
+     * @param b end of interval.
+     * @param tol tolerance of error.
+     * @param n number of iterations so method doesnt run forever.
+     * */
+    public BisectionMethod(double a, double b, double tol, int n){
+        endpointA = a;
+        endpointB = b;
+        tolerance = tol;
+        N = n;
+
+        findRoot();
+
+    }
+    public BisectionMethod(int a, int b, double tol, int n){
+        new BisectionMethod((double)a, (double)b, tol, n);
+    }
+    public void findRoot(){
+        int i = 1;
+        double FA = f(endpointA);
+        double Fr;
+        double tempRoot;
+
+
+        while(i<=N){
+            tempRoot = endpointA + (endpointB - endpointA)/2.0;
+            Fr = f(tempRoot);
+
+            if (Double.compare(Fr, 0) == 0 || ((endpointB - endpointA)/2.0) < tolerance){
+                root = tempRoot;
+                System.out.println(root);
+                break;
+            }
+
+            i = i+1;
+            if(FA*Fr > 0){
+                endpointA = tempRoot;
+                FA = Fr;
+            } else {
+                endpointB = tempRoot;
+            }
+        }
+
+        if(root == null){
+            System.out.println("Method failed after " + N + " iterations");
+        }
+    }
+
+    //f method f is a function
+    public double f(double x){ return x*x - 3*x - 4; }
+
+    public static void main(String[] args) {
+        new BisectionMethod(2.1, 15.5, 0.000000001, 50000);
+    }
+}
